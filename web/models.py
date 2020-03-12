1f4af73b46e43431fc9b4580e8174a06d8c0adc9
Kyeongdong Seo <seokyeongdong@gmail.com>
2020-03-12 18:27:29 +0900

    first commit

---
+++ web/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Site(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    status = models.IntegerField(default=200)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
