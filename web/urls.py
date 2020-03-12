26ae6e5becab6ee609edd04492794414d028ef2d
Kyeongdong Seo <seokyeongdong@gmail.com>
2020-03-13 00:59:51 +0900

    .

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.site_list, name='site_list'),
+    path('site/<int:pk>/', views.site_detail, name='site_detail'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Site
 
 
 def site_list(request):
-    return render(request, 'web/site_list.html', {})
+    sites = Site.objects.filter(status=200).order_by('-created_date')
+    return render(request, 'web/site_list.html', {'sites': sites})
+
+def site_detail(request, pk):
+    site = get_object_or_404(Site, pk=pk)
+    return render(request, 'web/site_detail.html', {'site': site})
