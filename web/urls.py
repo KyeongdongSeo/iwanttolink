eb756a8bddae9b4411380227953ce92e2e6fa53c
Kyeongdong Seo <seokyeongdong@gmail.com>
2020-03-13 01:53:12 +0900

    added views to create/edit

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.site_list, name='site_list'),
     path('site/<int:pk>/', views.site_detail, name='site_detail'),
+    path('site/new/', views.site_new, name='site_new'),
+    path('site/<int:pk>/edit/', views.site_edit, name='site_edit'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Site
+from .forms import SiteForm
 
 
 def site_list(request):
@@ -10,3 +11,28 @@ def site_list(request):
 def site_detail(request, pk):
     site = get_object_or_404(Site, pk=pk)
     return render(request, 'web/site_detail.html', {'site': site})
+
+def site_new(request):
+    if request.method == "POST":
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            site = form.save(commit=False)
+            site.created_date = timezone.now()
+            site.save()
+            return redirect('site_detail', pk=site.pk)
+    else:
+        form = SiteForm()
+    return render(request, 'web/site_edit.html', {'form': form})
+
+def site_edit(request, pk):
+    site = get_object_or_404(Site, pk=pk)
+    if request.method == "POST":
+        form = SiteForm(request.POST, instance=site)
+        if form.is_valid():
+            site = form.save(commit=False)
+            site.created_date = timezone.now()
+            site.save()
+            return redirect('site_detail', pk=site.pk)
+    else:
+        form = SiteForm(instance=site)
+    return render(request, 'web/site_edit.html', {'form': form})
